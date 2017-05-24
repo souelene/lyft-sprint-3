cff5b284f767c6005bbe726a465aeb649de76361
souelene <hola.sou.elene@gmail.com>
2017-05-24 09:37:53 -0400

    se termina html y css, funcionalidad en proceso

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,46 @@
+
+var x=document.getElementById("x").value;
+var y=document.getElementById("y").value;
+
+var bStart=document.getElementById("start");
+
+bStart.addEventListener('click',
+function partir() {
+    function Validar(){
+    	if(x>10||y>6){
+    		return alert("Las coordenadas deben estar en el rango de 10x6");
+    	}
+    	else{
+			var hijoBorra=document.getElementById("borra");
+			var contenedor=document.getElementById("boxAuto");
+			return contenedor.removeChild(hijoBorra);
+    	}
+    };
+	Validar();
+
+	
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 document.getElementById("bt").addEventListener('click', telefono, false);
 function telefono() {
     alert("Teléfono: +569 99689 5236");   
